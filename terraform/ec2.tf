b8d3f96d4061b9b1a49fdbd9c158db13d5e4249c
darshitsri <119929681+darshitsri@users.noreply.github.com>
2023-02-03 18:01:27 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "terraform_instance" {
-  ami           = "ami-06984ea821ac0a879"
+  ami           = "ami-01ccff0fc8b9448a6"
   instance_type = "t2.micro"
   key_name = "key-pem"
 
